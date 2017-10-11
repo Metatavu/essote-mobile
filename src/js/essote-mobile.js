cf4e7c76cd043162ba6a2d86b7ab86987a28afa2
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-10-11 14:51:43 +0300

    Added emergency room page

--- src/js/essote-mobile.js
+++ src/js/essote-mobile.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6 */
-/* global getConfig, pugSubMenu */
+/* global getConfig, pugSubMenu, pugNewsPage, pugEmergencyRoomPage */
 
 (function(){
   'use strict';
@@ -25,7 +25,7 @@
           {target: 'tiedotteet-toimipisteet', icon: 'icon-toimipisteet', name: 'Lorem Ipsum', level: 2}
         ],
         'tiedotteet-ota-yhteytta': {
-          newsImage: '/gfx/news-image-1.jpeg',
+          newsImage: 'gfx/news-image-1.jpeg',
           title: 'Lorem Ipsum',
           content: '<p>In mollis lectus vitae ipsum aliquam, vel dapibus magna volutpat. Donec luctus tempor odio lacinia luctus. Duis pulvinar orci tristique laoreet cursus. Proin gravida quam eget massa euismod, in consectetur libero ullamcorper. Sed tempor dolor nec gravida cursus. Quisque vehicula leo in erat gravida, id volutpat sapien aliquam. Nullam non bibendum mi, quis vestibulum felis. Mauris cursus velit ac gravida bibendum.</p><p>Nam condimentum non lorem sed posuere. Vivamus ultricies consequat leo luctus pulvinar. In eget odio varius, auctor mi vitae, tincidunt leo. Vestibulum tincidunt varius imperdiet. In in sem lorem. Praesent hendrerit vehicula sapien in pharetra. In nec sagittis dolor. Sed quis metus pharetra, dictum enim ut, porttitor ante. Praesent id dolor ex.</p>'
         }
@@ -36,8 +36,8 @@
       this.createIndexPage();
       this._swiper = new Swiper('.swiper-container', { });
       this.subpages = {};
-      
       this.subpages['tiedotteet'] = new EssoteMobileSubPage({title: 'Tiedotteet', links: this.options.subpageData['tiedotteet'] }, pugSubMenu);
+      this.subpages['paivystys'] = new EssoteMobileSubPage({title: 'PÄIVYSTYS'}, pugEmergencyRoomPage);
       this.subpages['tiedotteet-ota-yhteytta'] = new EssoteMobileSubPage(this.options.subpageData['tiedotteet-ota-yhteytta'] , pugNewsPage);
       
       $(this.element).on('touchend', '.back-btn', $.proxy(this._onBackBtnTouchEnd, this));
@@ -50,6 +50,7 @@
       $('.list-link').removeClass('active');
       const listItemTarget = $(e.target).closest('.list-link').addClass('active').attr('data-target');
       const listItemLevel = parseInt($(e.target).closest('.list-link').attr('data-level'), 10);
+      console.log(listItemLevel);
       this.createSubPage(listItemTarget, listItemLevel);
     },
     
@@ -76,6 +77,7 @@
       for(let i = level; i <= this.options.maxLevel; i++) {
         slidesToRemove.push(i);
       }
+      console.log(slidesToRemove);
       this._swiper.removeSlide(slidesToRemove);
       this._swiper.appendSlide(subPageHtml);
     }
@@ -83,6 +85,7 @@
   });
   
   $(document).on("deviceready", () => {
+    console.log('Starting');
     $(document.body).essoteMobile();
   });
 
