8ed66f2bff777fc5755c973571182bc73f25b771
Antti Leppä <antti.leppa@metatavu.fi>
2018-05-21 19:42:10 +0300

    Fixed issues with building api-client

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 /* jshint esversion: 6 */
 /*global module:false*/
 
-const _ = require('lodash');
-const fs = require('fs');
-const util = require('util');
-const pug = require('pug');
-const path = require('path');
+const _ = require("lodash");
+const fs = require("fs");
+const util = require("util");
+const pug = require("pug");
+const path = require("path");
 
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
+  require("load-grunt-tasks")(grunt);
   
-  grunt.registerMultiTask('compile-client-templates', 'Compiles client pug templates', function () {
+  grunt.registerMultiTask("compile-client-templates", "Compiles client pug templates", function () {
     const clientTemplates = fs.readdirSync(this.data.sourceFolder);
     const compiledClientTemplates = [];
     
     for (let i = 0; i < clientTemplates.length; i++) {
-      let baseName = clientTemplates[i].replace('.pug', '');
+      let baseName = clientTemplates[i].replace(".pug", "");
       baseName = `${baseName[0].toUpperCase()}${baseName.substring(1)}`;
       const templateName = _.camelCase(`${this.data.templatePrefix}${baseName}`);
       compiledClientTemplates.push(pug.compileFileClient(this.data.sourceFolder + clientTemplates[i], { name: templateName, compileDebug: false }));
@@ -27,10 +27,10 @@ module.exports = function(grunt) {
       fs.mkdirSync(destDir);
     }
     
-    fs.writeFileSync(this.data.destFile, compiledClientTemplates.join(''));
+    fs.writeFileSync(this.data.destFile, compiledClientTemplates.join(""));
   });
   
-  grunt.registerMultiTask('generate-config', 'Generates config.js', function() {
+  grunt.registerMultiTask("generate-config", "Generates config.js", function() {
     const config = this.data.options.config;
     
     const values = {
@@ -38,67 +38,77 @@ module.exports = function(grunt) {
       wnsPusher: config.wnsPusher
     };
     
-    fs.writeFileSync(this.data.options.output, util.format('function getConfig() { return %s; };', JSON.stringify(values)));
+    fs.writeFileSync(this.data.options.output, util.format("function getConfig() { return %s; };", JSON.stringify(values)));
     
     return true;
   });
   
   grunt.initConfig({
-    'sass': {
+    "shell": {
+      "install-api-client": {
+        "command": "npm install",
+        "options": {
+          "execOptions": {
+            "cwd": `${__dirname}/src/api-client`
+          }
+        }
+      }
+    },
+    "sass": {
       dist: {
         options: {
-          style: 'compressed'
+          style: "compressed"
         },
         files: [{
           expand: true,
-          cwd: 'src/scss',
-          src: ['*.scss'],
-          dest: 'www/css',
-          ext: '.min.css'
+          cwd: "src/scss",
+          src: ["*.scss"],
+          dest: "www/css",
+          ext: ".min.css"
         }]
       }
     },
-    postcss: {
-        options: {
-          map: true,
-          processors: [ require('autoprefixer') ]
-        },
-        dist: {
-          src: 'www/css/*.css'
-        }
-	},
-    'pug': {
+    "postcss": {
+      options: {
+        map: true,
+        processors: [ require("autoprefixer") ]
+      },
+      dist: {
+        src: "www/css/*.css"
+      }
+	  },
+    "pug": {
       compile: {
         options: {
           data: function(dest, src) {
-            return require('./config.json');
+            return require("./config.json");
           }
         },
         files: [{
           expand: true,
-          cwd: 'src/templates',
-          src: ['*.pug'],
-          dest: 'www',
-          ext: '.html'
+          cwd: "src/templates",
+          src: ["*.pug"],
+          dest: "www",
+          ext: ".html"
         }]
       }
     },
-    'compile-client-templates': {
-      'compile': {
-        'sourceFolder': __dirname + '/src/client-templates/',
-        'destFile': __dirname + '/www/js/pug-templates.js',
-        'templatePrefix': 'pug'
+    "compile-client-templates": {
+      "compile": {
+        "sourceFolder": __dirname + "/src/client-templates/",
+        "destFile": __dirname + "/www/js/pug-templates.js",
+        "templatePrefix": "pug"
       }
     },
-    'generate-config': {
-      'generate': {
-        'options': {
-          'config': require('./config.json'),
-          'output': 'www/js/config.js'
+    "generate-config": {
+      "generate": {
+        "options": {
+          "config": require("./config.json"),
+          "output": "www/js/config.js"
         }
       }
     },
-    'babel': {
+    "babel": {
       options: {
         sourceMap: true,
         minified: false
@@ -106,29 +116,29 @@ module.exports = function(grunt) {
       dist: {
         files: [{
           expand: true,
-          cwd: 'src/js',
-          src: ['*.js'],
-          dest: 'www/js/',
-          ext: '.js'
+          cwd: "src/js",
+          src: ["*.js"],
+          dest: "www/js/",
+          ext: ".js"
         }]
       }
     },
-    bower: {
+    "bower": {
       install: { }
     },
-    wiredep: {
+    "wiredep": {
       target: {
-        src: 'www/index.html'
+        src: "www/index.html"
       }
     },
     browserify: {
       apiclient: {
         files: {
-          'www/js/soteapi-client.js': ['src/api-client/main.js']
+          "www/js/soteapi-client.js": ["src/api-client/main.js"]
         }
       }
     }
   });
   
-  grunt.registerTask('default', [ 'sass', 'postcss', 'pug', 'compile-client-templates', 'generate-config', 'babel', 'bower', 'wiredep', 'browserify:apiclient' ]);
+  grunt.registerTask("default", [ "shell:install-api-client", "sass", "postcss", "pug", "compile-client-templates", "generate-config", "babel", "bower", "wiredep", "browserify:apiclient" ]);
 };
